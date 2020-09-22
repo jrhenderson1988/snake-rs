ea6172d0b5a5cc1dfa0a850c3916cdbf9f15b270
Jonathon Henderson <jrhenderson1988@hotmail.com>
2020-09-22 09:41:56 +0100

    Correctly render snake corners

--- src/game.rs
+++ src/game.rs
@@ -106,6 +106,7 @@ impl Game {
 
         self.restore_ui();
 
+
         println!("Game Over! Your score is {}", self.score);
     }
 
@@ -198,40 +199,62 @@ impl Game {
     fn draw_snake(&mut self) {
         let fg = SetForegroundColor(match self.speed % 3 {
             0 => Color::Green,
-            1 => Color::Yellow,
-            _ => Color::Blue
+            1 => Color::Cyan,
+            _ => Color::Yellow
         });
         self.stdout.execute(fg).unwrap();
 
-        for body in self.snake.get_body_points() {
+        let body_points = self.snake.get_body_points();
+        for (i, body) in body_points.iter().enumerate() {
+            let previous = if i == 0 { None } else { body_points.get(i - 1) };
+            let next = body_points.get(i + 1);
+            let symbol = if let Some(&next) = next {
+                if let Some(&previous) = previous {
+                    if previous.x == next.x {
+                        '║'
+                    } else if previous.y == next.y {
+                        '═'
+                    } else {
+                        let d = body.apply_delta((0, 1));
+                        let r = body.apply_delta((1, 0));
+                        let u = if body.y == 0 { body.clone() } else { body.apply_delta((0, -1)) };
+                        let l = if body.x == 0 { body.clone() } else { body.apply_delta((-1, 0)) };
+                        if (next == d && previous == r) || (previous == d && next == r) {
+                            '╔'
+                        } else if (next == d && previous == l) || (previous == d && next == l) {
+                            '╗'
+                        } else if (next == u && previous == r) || (previous == u && next == r) {
+                            '╚'
+                        } else {
+                            '╝'
+                        }
+                    }
+                } else {
+                    'O'
+                }
+            } else if let Some(&previous) = previous {
+                if body.y == previous.y {
+                    '═'
+                } else {
+                    '║'
+                }
+            } else {
+                panic!("Invalid snake body point.");
+            };
+
             self.stdout
                 .execute(MoveTo(body.x + 1, body.y + 1)).unwrap()
-                .execute(Print("o")).unwrap();
+                .execute(Print(symbol)).unwrap();
         }
-
-        let head_point = self.snake.get_head_point();
-        self.stdout
-            .execute(MoveTo(head_point.x + 1, head_point.y + 1)).unwrap()
-            .execute(
-                Print(
-                    "O"
-                    // match self.snake.get_direction() {
-                    //     Direction::North => "v".to_string(),
-                    //     Direction::East => "<".to_string(),
-                    //     Direction::South => "^".to_string(),
-                    //     Direction::West => ">".to_string(),
-                    // }
-                )
-            ).unwrap();
     }
 
     fn draw_food(&mut self) {
-        self.stdout.execute(SetForegroundColor(Color::Red)).unwrap();
+        self.stdout.execute(SetForegroundColor(Color::White)).unwrap();
 
         for food in self.food.iter() {
             self.stdout
                 .execute(MoveTo(food.x + 1, food.y + 1)).unwrap()
-                .execute(Print("*")).unwrap();
+                .execute(Print("•")).unwrap();
         }
     }
 
